93edf661e476f90c67bf1d0cd1399affd538b341
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-10-31 10:39:14 +0700

    Update afk.py

--- Yukki/modules/afk.py
+++ Yukki/modules/afk.py
@@ -34,35 +34,35 @@ async def active_afk(_, message: Message):
             seenago = get_readable_time((int(time.time() - timeafk)))
             if afktype == "text":
                 send = await message.reply_text(
-                    f"**{message.from_user.first_name}**kembali online {seenago}",
+                    f"**{message.from_user.first_name}** kembali online {seenago}",
                     disable_web_page_preview=True,
                 )
             if afktype == "text_reason":
                 send = await message.reply_text(
-                    f"**{message.from_user.first_name}**Kembali online {seenago}\n\n**Alasan:** `{reasonafk}`",
+                    f"**{message.from_user.first_name}** Kembali online {seenago}\n\n**Alasan:** `{reasonafk}`",
                     disable_web_page_preview=True,
                 )
             if afktype == "animation":
                 if str(reasonafk) == "None":
                     send =  await message.reply_animation(
                         data,
-                        caption=f"**{message.from_user.first_name}**Kembali online {seenago}",
+                        caption=f"**{message.from_user.first_name}** Kembali online {seenago}",
                     )
                 else:
                     send = await message.reply_animation(
                         data,
-                        caption=f"**{message.from_user.first_name}**Kembali online {seenago}\n\n**Alasan:** `{reasonafk}`",
+                        caption=f"**{message.from_user.first_name}** Kembali online {seenago}\n\n**Alasan:** `{reasonafk}`",
                     )
             if afktype == "photo":
                 if str(reasonafk) == "None":
                     send = await message.reply_photo(
                         photo=f"downloads/{user_id}.jpg",
-                        caption=f"**{message.from_user.first_name}**Kembali online {seenago}",
+                        caption=f"**{message.from_user.first_name}** Kembali online {seenago}",
                     )
                 else:
                     send = await message.reply_photo(
                         photo=f"downloads/{user_id}.jpg",
-                        caption=f"**{message.from_user.first_name}**Kembali online {seenago}\n\n**Alasan:** `{reasonafk}`",
+                        caption=f"**{message.from_user.first_name}** Kembali online {seenago}\n\n**Alasan:** `{reasonafk}`",
                     )
         except Exception as e:
             send =  await message.reply_text(
