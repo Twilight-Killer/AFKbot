b137ac11e10817f67c0993c221db8bc901b4ff72
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-10-31 08:34:54 +0700

    Update start.py

--- Yukki/modules/start.py
+++ Yukki/modules/start.py
@@ -69,7 +69,7 @@ async def on_close_button(client, CallbackQuery):
 
 @app.on_callback_query(filters.regex("cleanmode_answer"))
 async def on_cleanmode_button(client, CallbackQuery):
-    await CallbackQuery.answer("⁉️ Apa ini?\n\nSaat diaktifkan, Bot akan menghapus pesannya setelah 5 Menit agar obrolan Anda bersih dan jelas.‌‌", show_alert=True)
+    await CallbackQuery.answer("❗Informasi.\nSaat diaktifkan, Bot akan menghapus pesannya setelah 5 Menit agar obrolan Anda bersih dan jelas.‌‌", show_alert=True)
 
 @app.on_callback_query(filters.regex("settings_callback"))
 async def on_settings_button(client, CallbackQuery):
