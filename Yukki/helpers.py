df0cbcf32d89858aa9fc1659159fdcdd1bf7a5bc
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-10-31 07:14:14 +0700

    Update helpers.py

--- Yukki/helpers.py
+++ Yukki/helpers.py
@@ -76,67 +76,38 @@ asyncio.create_task(auto_clean())
 
 
 RANDOM = [
-    "https://telegra.ph//file/0879fbdb307005c1fa8ab.jpg",
-    "https://telegra.ph//file/19e3a9d5c0985702497fb.jpg",
-    "https://telegra.ph//file/b5fa277081dddbddd0b12.jpg",
-    "https://telegra.ph//file/96e96245fe1afb82d0398.jpg",
-    "https://telegra.ph//file/fb140807129a3ccb60164.jpg",
-    "https://telegra.ph//file/09c9ea0e2660efae6f62a.jpg",
-    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
-    "https://telegra.ph//file/efb26cc17eef6fe82d910.jpg",
-    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
-    "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
-    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
-    "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
-    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
-    "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
-    "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
-    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
-    "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
-    "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
-    "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
-    "https://telegra.ph//file/bc97b1e9bbe6d6db36984.jpg",
-    "https://telegra.ph//file/2ddf3521636d4b17df6dd.jpg",
-    "https://telegra.ph//file/72e4414f618111ea90a57.jpg",
-    "https://telegra.ph//file/a958417dcd966d341bfe2.jpg",
-    "https://telegra.ph//file/0afd9c2f70c6328a1e53a.jpg",
-    "https://telegra.ph//file/82ff887aad046c3bcc9a3.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/a7cba6e78bb63e1b4aefb.jpg",
+
     "https://telegra.ph//file/f8ba75bdbb9931cbc8229.jpg",
     "https://telegra.ph//file/07bb5f805178ec24871d3.jpg"
 ]
 
 
-HELP_TEXT = f"""Welcome to {botname}'s Help Section.
-
-- When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.
+HELP_TEXT = f"""Selamat datang di {botname}'s Bagian Bantuan‌‌
 
 
-/afk - This will set you offline.
+/afk - Ini akan membuat Anda offline‌‌.
 
-/afk [Reason] - This will set you offline with a reason.
+/afk [Alasan] - Ini akan membuat Anda offline dengan suatu alasan‌‌.
 
-/afk [Replied to a Sticker/Photo] - This will set you offline with an image or sticker.
+/afk [Membalas Sticker/Photo] - Ini akan membuat Anda offline dengan gambar atau stiker‌‌.
 
-/afk [Replied to a Sticker/Photo] [Reason] - This will set you afk with an image and reason both.
+/afk [Membalas Sticker/Photo] [Alasan] - Ini akan membuat Anda offline dengan gambar dan alasan keduanya.‌‌
 
 
-/settings - To change or edit basic settings of AFK Bot.
+/settings - Untuk mengubah atau mengedit pengaturan dasar Offine Bot.‌‌
 """
 
 def settings_markup(status: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="🔄 Clean Mode", callback_data="cleanmode_answer"),
+            InlineKeyboardButton(text="🔄 Mode bersih", callback_data="cleanmode_answer"),
             InlineKeyboardButton(
-                text="✅ Enabled" if status == True else "❌ Disabled",
+                text="✅ Tampilkan" if status == True else "❌ Sembunyikan",
                 callback_data="CLEANMODE",
             ),
         ],
         [
-            InlineKeyboardButton(text="🗑 Close Menu", callback_data="close"),
+            InlineKeyboardButton(text="🗑 Tutup Menu", callback_data="close"),
         ],
     ]
     return buttons
