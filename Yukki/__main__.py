b9636b2dd78a737d379e01cfd3cdf3c4a9610791
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2023-11-14 16:37:13 +0700

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -21,9 +21,9 @@ loop = asyncio.get_event_loop()
 async def initiate_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("Yukki.modules." + all_module)
-    print("Started AFKbot.")
+    print("AFKbot Started.")
     await idle()
-    print("Stopping Bot. GoodBye!")
+    print("Bot Stopped. GoodBye!")
 
 
 if __name__ == "__main__":
